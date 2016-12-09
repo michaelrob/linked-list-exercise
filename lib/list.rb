c6c8dcd2c11880b1ef893968a39d4e52a50cffac
Michael Robinson <seraph890@gmail.com>
2016-12-09 12:00:01 +0900

    fixed up elements method, added comments to sort method and fleshed out member method

--- lib/list.rb
+++ lib/list.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class List
   include Enumerable
 
@@ -28,7 +30,7 @@ class List
 
   # returns all elements in list
   def elements
-    @options[:sorted] == 1 ? sorted(entries) : entries
+    @options[:sorted] == 1 ? sort(entries) : entries.reverse
   end
 
   # returns true if the list is empty, false otherwise
@@ -43,16 +45,22 @@ class List
 
   # returns true if object is a member of the list, false otherwise
   def member?(object)
+    any? { |o| o == object }
   end
 
   # returns and removes the first element of the list; or nil if the list is empty
   def pop
-    nil
+    return nil if @node.nil?
+
+    @node = @node.next
   end
 
 private
 
-  def sorted(list)
+  # returns sorted list, in the future we may want to add additional arguments
+  # for more complicated sorting
+  def sort(list)
+    list.sort
   end
 
 end
